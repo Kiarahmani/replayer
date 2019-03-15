14e667006d1bd3c283fb0d7a6233f88b5f809a20
Kiarahmani <rahmank@purdue.edu>
2019-03-15 13:45:01 -0400

    transactions talk to cassandra

--- replayer/src/main/java/com/github/kiarahmani/replayer/Client.java
+++ replayer/src/main/java/com/github/kiarahmani/replayer/Client.java
@@ -1,8 +1,10 @@
 package com.github.kiarahmani.replayer;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
@@ -21,24 +23,36 @@ public class Client {
 		p.setProperty("id", String.valueOf(this.id));
 	}
 
-	public long transactionOne(int id) {
-		for (int i = 0; i < 800; i++) {
-			System.out.println(i+": I am transactionOne! id:" + id);
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+	private void close() {
+		try {
+			connect.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+	}
+
+	public long transactionOne(int id) throws Exception {
+
+		Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
+		try {
+			connect = DriverManager.getConnection("jdbc:cassandra://localhost" + ":1904" + id + "/testks");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("successfully connected to Cassandra! \n" + connect);
+		preparedStatement = connect.prepareStatement("SELECT balance FROM accounts WHERE id = 2");
+		rs = preparedStatement.executeQuery();
+		rs.next();
+		int balance = rs.getInt("balance");
+		System.out.println("the balance for the read account is: " + balance);
+		close();
+
 		return 0;
 	}
-	
-	
-	
-	
+
 	public long transactionTwo(int id) {
-		for (int i = 0; i < 800; i++) {
-			System.out.println(i+": I am transactionTwo! id:" + id);
+		for (int i = 0; i < 50; i++) {
+			System.out.println(i + ": I am transactionTwo! id:" + id);
 			try {
 				Thread.sleep(10);
 			} catch (InterruptedException e) {
